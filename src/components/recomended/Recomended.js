99e02e938451d63f8ca9222e16c94948f50e7253
dana cohen <danah2139@gmail.com>
2021-04-04 19:59:31 +0300

    refactoring

--- src/components/recomended/Recomended.js
+++ src/components/recomended/Recomended.js
@@ -2,10 +2,14 @@ import { useState, useEffect } from 'react';
 
 const Recomended = (props) => {
 	useEffect(() => {
-		navigator.geolocation.getCurrentPosition((position) => {
-			console.log('Latitude is :', position.coords.latitude);
-			console.log('Longitude is :', position.coords.longitude);
-		});
+		navigator.geolocation.getCurrentPosition(
+			(position) => {
+				console.log(position);
+			},
+			(error) => {
+				console.error('Error Code = ' + error.code + ' - ' + error.message);
+			}
+		);
 	}, []);
 	console.log('hi', props);
 	return <div>Recomended</div>;
