c9d835e4044f418d2eefde29d5b79fc71f3d09f2
dana cohen <danah2139@gmail.com>
2021-04-06 16:37:47 +0300

    add form

--- src/components/recomended/Recomended.js
+++ src/components/recomended/Recomended.js
@@ -1,7 +1,8 @@
 import { useState, useEffect } from 'react';
 import { Wrapper } from './recomended.styled';
 
-const Recomended = (props) => {
+const Recomended = ({ routs }) => {
+	const [formInfo, setFormInfo] = useState({});
 	useEffect(() => {
 		navigator.geolocation.getCurrentPosition(
 			(position) => {
@@ -12,52 +13,113 @@ const Recomended = (props) => {
 			}
 		);
 	}, []);
+	const renderRegions = () => {
+		let regionsArr = routs
+			.filter((route) => route.Region.search(/[^a-z|A-Z| ]/) === -1)
+			.map((route) => ({ region: route.Region, id: route._id }));
+		regionsArr = regionsArr.filter(
+			(regionItem, index, arr) =>
+				index === arr.findIndex((r) => r.region === regionItem.region)
+		);
+
+		return regionsArr.map((regionItem) => (
+			<option key={regionItem.id} value={regionItem.region}>
+				{regionItem.region}
+			</option>
+		));
+	};
+
+	const handleInputChange = (event) => {
+		setFormInfo({ ...formInfo, [event.target.name]: event.target.value });
+	};
 
 	const handleSearch = (event) => {
 		event.preventDefault();
+		console.log(formInfo);
 	};
-	console.log('hi', props);
 	return (
 		<Wrapper>
 			<form onSubmit={handleSearch}>
 				<label>
 					Insert Duration of Your trip:{' '}
-					<input type="text" name="" onChange="" value="" />
+					<input type="text" name="Duration" onChange={handleInputChange} />
 				</label>
 				<label>
 					Choose Region:{' '}
-					<select>
+					<select name="Region" onChange={handleInputChange}>
 						<option>Select Region</option>
+						{renderRegions()}
 					</select>
 				</label>
+				<label>Trail Type:</label>
 				<div>
 					<label>
 						<input
 							type="radio"
 							value="Cycling"
 							//checked={state.selectedOption === 'Male'}
-							///onChange={onValueChange}
+							name="Trail_Type"
+							onChange={handleInputChange}
 						/>
 						Cycling
 					</label>
 				</div>
-				<label>
-					Trail Type:
-					<input type="radio" name="type" value="Walking" />
-					Walking
-					<input type="radio" value="4x4" name="type" />
-					4x4
-					<input type="radio" value="VehicleRoute" name="type" />
-					Vehicle Route
-				</label>
-				<label>
-					Parking:
-					<input type="radio" />
-					Yes
-					<input type="radio" />
-					No
-				</label>
-
+				<div>
+					<label>
+						<input
+							type="radio"
+							name="Trail_Type"
+							value="Walking"
+							onChange={handleInputChange}
+						/>
+						Walking
+					</label>
+				</div>
+				<div>
+					<label>
+						<input
+							type="radio"
+							name="Trail_Type"
+							value="4x4"
+							onChange={handleInputChange}
+						/>
+						4x4
+					</label>
+				</div>
+				<div>
+					<label>
+						<input
+							type="radio"
+							name="Trail_Type"
+							value="VehicleRoute"
+							onChange={handleInputChange}
+						/>
+						Vehicle Route
+					</label>
+				</div>
+				<label>Parking:</label>
+				<div>
+					<label>
+						<input
+							type="radio"
+							name="Parking"
+							value="Yes"
+							onChange={handleInputChange}
+						/>
+						Yes
+					</label>
+				</div>
+				<div>
+					<label>
+						<input
+							type="radio"
+							name="Parking"
+							value="No"
+							onChange={handleInputChange}
+						/>
+						No
+					</label>
+				</div>
 				<input type="submit" value="Search" />
 			</form>
 		</Wrapper>
